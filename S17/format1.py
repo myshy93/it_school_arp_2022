80bf3221261d197d251e39faba06811dc2c0fc3b
Dinu Mihai <mihai.dinu93@gmail.com>
2022-06-16 21:11:50 +0300

    f strings

--- S17/format1.py
+++ S17/format1.py
@@ -12,8 +12,26 @@ age = 20
 print("Prenume: %s | Nume: %s | Varsta: %d" % (last_name, first_name, age))
 
 
-class Client:
+def get_titles(lang):
 
+    if lang == "ro":
+        return {
+            "title": "factura",
+            "client_name": "nume client",
+            "client_address": "adresa",
+            "iban": "IBAN",
+        }
+
+    if lang == "en":
+        return {
+            "title": "invoice",
+            "client_name": "client name",
+            "client_address": "address",
+            "iban": "account number",
+        }
+
+
+class Client:
     def __init__(self, name, address, iban) -> None:
         self.name = name
         self.address = address
@@ -21,17 +39,29 @@ class Client:
 
 
 class Invoice:
-
     def __init__(self, client) -> None:
         self.client = client
 
-    def print_invoice(self, title):
-        print("{:*^50}".format(title))
-        print("Nume client: {:>37}".format(self.client.name))
-        print("Adresa: {:>42}".format(self.client.address))
-        print("IBAN: {:>44.13}".format(self.client.iban))
-        print("{:=^50}".format(str(datetime.now())))
+    def print_invoice(self, lang):
+        inv_len = 70
+        strings = get_titles(lang)
+
+        print(f"{strings['title'].title():*^{inv_len}}")
+        print(
+            f"{strings['client_name'].title()}:"
+            f"{self.client.name:>{inv_len - 1 - len(strings['client_name'])}}"
+        )
+        print(
+            f"{strings['client_address'].title()}:"
+            f"{self.client.address:>{inv_len - 1 - len(strings['client_address'])}}"
+        )
+        print(
+            f"{strings['iban'].title()}:"
+            f"{self.client.iban:>{inv_len - 1 - len(strings['iban'])}.20}"
+        )
+        print(f"{datetime.now().strftime('%d-%m-%Y'):=^{inv_len}}")
         print()
+
 
 client = Client("George Pascu", "Bucuresti", "RO76INGB00000000")
 client1 = Client("Ion Pascu", "Iasi", "RO76INGB000000124300")
@@ -39,5 +69,5 @@ client1 = Client("Ion Pascu", "Iasi", "RO76INGB000000124300")
 invoice1 = Invoice(client)
 invoice2 = Invoice(client1)
 
-invoice1.print_invoice("Factura")
-invoice2.print_invoice("Invoice")
+invoice1.print_invoice("ro")
+invoice2.print_invoice("en")
---
+++ S17/format3.py
@@ -0,0 +1,11 @@
+
+first_name = "Mihai"
+last_name = "Dinu"
+age = 20
+
+user = {
+    "name": "Mihai",
+}
+
+print(f"Nume: {user['name']} | Varsta: {age:*^{30 -2}}")
+

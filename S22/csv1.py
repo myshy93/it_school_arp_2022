3194f888baa7230f1cd08d57c3a120e6b151557c
Dinu Mihai <mihai.dinu93@gmail.com>
2022-07-05 20:39:30 +0300

    data update

--- S22/csv1.py
+++ S22/csv1.py
@@ -14,32 +14,31 @@ try:
 except OSError:
     print("File error.")
 
-# print(f"Total salarii: {sum(lista_salarii)}")
+print(f"Total salarii: {sum(lista_salarii)}")
 
-# # citire fisier csv cu nume campuri
-# field_names = [
-#     "first_name",
-#     "last_name",
-#     "id",
-#     "gros_salary",
-#     "days_off"
-# ]
-# try:
-#     with open(files_path.joinpath("salarii.csv")) as fin:
-#         # list -> extrage datele din generator
-#         dict_reader = csv.DictReader(fin, fieldnames=field_names)
-#         for i in dict_reader:
-#             lista_salarii.append(float(i['gros_salary']))
-# except OSError:
-#     print("File error.")
+# citire fisier csv cu nume campuri
+field_names = [
+    "first_name",
+    "last_name",
+    "id",
+    "gros_salary",
+    "days_off"
+]
+try:
+    with open(files_path.joinpath("salarii.csv")) as fin:
+        # list -> extrage datele din generator
+        dict_reader = csv.DictReader(fin, fieldnames=field_names)
+        for i in dict_reader:
+            lista_salarii.append(float(i['gros_salary']))
+except OSError:
+    print("File error.")
 
-# print(sum(lista_salarii))
-
+print(sum(lista_salarii))
 
 # scriere csv
 try:
     with open(files_path.joinpath("net_salaries.csv"), "w") as fout:
-        csv_writer = csv.writer(fout)
+        csv_writer = csv.writer(fout, lineterminator="\n")
         for i in reader:
             net = float(i[3]) * 0.65
             csv_writer.writerow([i[0], i[1], f"{net:.2f}"])
--- S22/xls.py
+++ S22/xls.py
@@ -5,17 +5,29 @@ from openpyxl import Workbook, load_workbook
 root = pathlib.Path(__file__).parent
 files_path = root.joinpath("files")
 
+cantitati = {}
+
 # read xlsx files
-wb = load_workbook(files_path.joinpath("inventory.xlsx"))
-print(wb.sheetnames)
+wb = load_workbook(files_path.joinpath("inventory.xlsx")) # deschide fisierul pentru citire
+# print(type(wb))
+# print(wb)
+# print(wb.sheetnames)
 sheet1 = wb['Sheet']
 
-print(sheet1['A1'].value)
 
+rows = list(sheet1.iter_rows()) # extragem toate liniile
+rows = rows[1:] # eliminam headerul
+
+for i in rows:
+    if int(i[2].value) < 30:
+        cantitati[i[0].value] = i[2].value
+
+print(cantitati)
+wb.close()
 
 
 # write xlsx files
-filename = "inventory_out.xlsx"
+filename = "list_cumparaturi.xlsx"
 
 # init workbook
 workbook = Workbook()
@@ -30,5 +42,9 @@ sheet["A2"] = "Vopsea"
 sheet["B2"] = "BUC"
 sheet["C2"] = 10
 
+list_sheet = workbook.create_sheet('list')
 
-# workbook.save(filename=files_path.joinpath(filename))
+for i in cantitati.keys():
+    list_sheet.append([i])
+
+workbook.save(filename=files_path.joinpath(filename))

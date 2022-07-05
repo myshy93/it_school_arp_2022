980434b77e7f12f520e819fb4c051d3b3940231d
Dinu Mihai <mihai.dinu93@gmail.com>
2022-07-05 19:42:01 +0300

    new data

--- S22/csv1.py
+++ S22/csv1.py
@@ -4,32 +4,36 @@ import pathlib
 root = pathlib.Path(__file__).parent
 files_path = root.joinpath("files")
 
-# citire fisier csv
+# # citire fisier csv
+lista_salarii = []
 try:
     with open(files_path.joinpath("salarii.csv")) as fin:
-        reader = list(csv.reader(fin))
+        reader = list(csv.reader(fin)) # csv.reader returneaza un generator, trebuie convertit la lista
+        for i in reader:
+            lista_salarii.append(float(i[3]))
 except OSError:
     print("File error.")
-else:
-    for i in reader:
-        print(i)
 
-# citire fisier csv cu nume campuri
-field_names = [
-    "first_name",
-    "last_name",
-    "id",
-    "gros_salary",
-    "days_off"
-]
-try:
-    with open(files_path.joinpath("salarii.csv")) as fin:
-        # list -> extrage datele din generator
-        dict_reader = list(csv.DictReader(fin, fieldnames=field_names))
-        for i in dict_reader:
-            print(i)
-except OSError:
-    print("File error.")
+# print(f"Total salarii: {sum(lista_salarii)}")
+
+# # citire fisier csv cu nume campuri
+# field_names = [
+#     "first_name",
+#     "last_name",
+#     "id",
+#     "gros_salary",
+#     "days_off"
+# ]
+# try:
+#     with open(files_path.joinpath("salarii.csv")) as fin:
+#         # list -> extrage datele din generator
+#         dict_reader = csv.DictReader(fin, fieldnames=field_names)
+#         for i in dict_reader:
+#             lista_salarii.append(float(i['gros_salary']))
+# except OSError:
+#     print("File error.")
+
+# print(sum(lista_salarii))
 
 
 # scriere csv
@@ -37,9 +41,9 @@ try:
     with open(files_path.joinpath("net_salaries.csv"), "w") as fout:
         csv_writer = csv.writer(fout)
         for i in reader:
-            net = int(i[3]) * 0.65
-            csv_writer.writerow([i[0], i[1], net])
+            net = float(i[3]) * 0.65
+            csv_writer.writerow([i[0], i[1], f"{net:.2f}"])
 except OSError:
     print("File write error.")
 
-# csv.DictWriter - get a dict for each line
+# # csv.DictWriter - get a dict for each line

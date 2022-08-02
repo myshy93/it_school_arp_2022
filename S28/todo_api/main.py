38d6a0ce7d4b58eefd5d2c1f9a1a716768358d9e
Dinu Mihai <mihai.dinu93@gmail.com>
2022-08-02 19:26:41 +0300

    html response

--- S28/todo_api/main.py
+++ S28/todo_api/main.py
@@ -2,14 +2,29 @@ import sqlite3
 import db
 from models import Todo
 
-from fastapi import FastAPI, Response, status
+from fastapi import FastAPI, Request, Response, status
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 # db init
 db_con = sqlite3.connect("db.sqlite")
 db.create_todo_table(db_con)
 # -------
+
+# html responses
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    context = {
+        "request": request,
+        "app_title": "Todos",
+        "todos": get_todos()
+    }
+    return templates.TemplateResponse("index.html", context)
+# -------------
 
 @app.post("/todo", name="Create TODO")
 def create_todo(todo: Todo):
@@ -29,7 +44,7 @@ def get_todos():
 
 
 @app.get("/todos/{id}")
-def get_todos(id):
+def get_todo(id):
     db_con = sqlite3.connect("db.sqlite")
     r = db.read_todo(db_con, id)
     r = list(r)
---
+++ project/P18_intretinere.md
@@ -0,0 +1,32 @@
+## Liste intretinere - CLI
+
+
+### Interfata:
+CLI
+
+#### Meniu
+1. Adauga bloc
+2. Adauga apartament
+3. Editeaza apartament
+4. Adauga contract
+5. Editeaza contract
+6. Adauga index apa rece
+
+
+### Actiuni
+#### Adauga bloc
+- trebuie creat un tabel in baza de date cu urmatoarele coloane
+    - id
+    - adresa
+    - nume_presedinte
+
+- se citesc de la tastatura toate informatiile necesare
+- se salveaza in baza de date
+
+#### Adauga apartament
+- trebuie creat un tabel in baza de date cu urmatoarele coloane
+    - id
+    - nr_ap
+    - nume_proprietar
+    - nr_locatari
+    - nr_camere
